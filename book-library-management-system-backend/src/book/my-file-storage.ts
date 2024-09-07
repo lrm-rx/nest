5c7834f6020534c4935d7ac292879cd42e3a0a57
Ming <756131502@qq.com>
2024-09-07 21:57:14 +0800

    图书模块开发 - 后端

---
+++ book-library-management-system-backend/src/book/my-file-storage.ts
@@ -0,0 +1,15 @@
+import * as multer from 'multer';
+import * as fs from 'fs';
+
+export const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    try {
+      fs.mkdirSync('uploads');
+    } catch (e) {}
+    cb(null, 'uploads');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}-${file.originalname}`;
+    cb(null, uniqueSuffix);
+  },
+});
