9eb04e6f27b5474a345fbb284e3e14b02b1e73d8
Ming <756131502@qq.com>
2024-09-07 23:29:39 +0800

    图书管理-前端用户注册/登录

--- book-management-system-frontend/src/main.tsx
+++ book-management-system-frontend/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { Login } from "./pages/Login/index.tsx";
+import { Register } from "./pages/Register/index.tsx";
+import { BookManage } from "./pages/BookManage/index.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const routes = [
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/",
+    element: <BookManage />,
+  },
+];
+const router = createBrowserRouter(routes);
+const root = createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(<RouterProvider router={router} />);
